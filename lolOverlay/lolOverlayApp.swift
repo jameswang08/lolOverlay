34e3a75c59014f8dc1d3937021733ab7310c20cd
James Wang <114769089+jameswang08@users.noreply.github.com>
2025-07-03 22:24:58 -0700

    Initial Commit

---
+++ lolOverlay/lolOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  lolOverlayApp.swift
+//  lolOverlay
+//
+//  Created by jam on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct lolOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
